ddeac2f615a7599fa3a074b8c24de2ec6a26f9e5
zahid-bracu <zahid.mym@gmail.com>
2021-08-15 00:44:39 +0600

    'ok'

--- pgd-bau/lecture.sql
+++ pgd-bau/lecture.sql
@@ -25,3 +25,28 @@ SELECT * FROM Advisor;
 
 --select column with condition
 SELECT s_ID, i_ID FROM ADVISOR WHERE s_ID>30000, i_ID>2000;
+
+
+--select with another condition
+select * from classroom where building='packard';
+
+
+---count values;
+select count(*) from classroom;
+
+--- limit the data
+select * from classroom limit 6;
+
+---skip the first one
+select * from classroom 2 offset 4;
+
+
+---show table information 
+show table status;
+
+--- show particuler table information
+show table status like 'classroom';
+
+
+--- describe 
+show create table classroom;
